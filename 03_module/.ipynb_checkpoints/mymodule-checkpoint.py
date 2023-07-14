c09d055fe62dcc2bd745f8b40f2d9d67db922f00
YukiteruKoide <darumasanyukiteru@gmail.com>
2023-07-14 11:28:10 +0900

    20230714-first-commit

---
+++ 03_module/.ipynb_checkpoints/mymodule-checkpoint.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+myvaribable = "This is global variable!"
+
+def myfunc():
+    print("This is my function!!")
+    
+def myfunc2():
+    print("This is my function2!!")
+
+
+# In[ ]:
+
+
+
+
